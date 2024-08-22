cd9f5d7e2079a6585323e55695c323e4267a135a
Mael Perreault <maperreault@cstj.qc.ca>
2024-08-22 16:56:15 -0400

    Cours 02 - Théorie

---
+++ Solution/Cours02_GitSyntaxe/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Cours02_GitSyntaxe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Mon nom est Mael!");
+
+            // Calcul et affichage de 4 variables
+            // camelCaseDesMots
+            int nombreEntier = 10 + 20;
+            nombreEntier = (nombreEntier + 1) * 10;
+
+            //int nombreEntier = 10 + 20;
+            double nombreVirgule = 10.123 + 20.235;
+            char unCaractere = '1';
+            string chaineCaracters = "Bonjour!";
+
+            Console.WriteLine(nombreEntier);
+            Console.WriteLine(nombreVirgule);
+            Console.WriteLine(unCaractere);
+            Console.WriteLine(chaineCaracters);
+        }
+    }
+}
